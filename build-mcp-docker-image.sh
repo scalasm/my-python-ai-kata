34a4b6883c663351fcc12a0a209962be81ca681c
Mario Scalas <mario.scalas@hmhco.com>
2025-06-24 15:13:29 +0000

    Build dockerized MCP server (very simple way)

---
+++ build-mcp-docker-image.sh
@@ -0,0 +1,9 @@
+#/#!/bin/bash
+set -ex
+poetry export --only main -f requirements.txt --output requirements.txt
+
+#TAG=$(git rev-parse --short HEAD)
+TAG=20250624
+echo "Building Docker image with tag: ${TAG}"
+docker build . -t scalasmhmh/my-hello-mcp-server:${TAG} -t scalasmhmh/my-hello-mcp-server:latest
+docker push scalasmhmh/my-hello-mcp-server:${TAG}
